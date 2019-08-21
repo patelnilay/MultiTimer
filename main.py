ad65ed3da0e2150d022b8b3a4cb77eb9cae00d9e
Luchs <github@meisterluchs.de>
2019-08-21 21:09:30 +0200

    added window

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import time
+import tkinter
+
+start_time = time.time()
 
 
 def time_calc(sec):
@@ -12,11 +15,29 @@ def time_calc(sec):
         minutes = "0" + minutes
     return "{}:{}:{}".format(hours, minutes, seconds)
 
-input("START?")
-start_time = time.time()
+
+top = tkinter.Tk()
+
+time1 = tkinter.StringVar()
+timer1 = tkinter.Label(top, textvariable=time1).grid(row=0, column=0)
+button1 = tkinter.Button().grid(row=1, column=0)
+
+time2 = tkinter.StringVar()
+timer2 = tkinter.Label(top, textvariable=time2).grid(row=0, column=1)
+button2 = tkinter.Button().grid(row=1, column=1)
+
+time3 = tkinter.StringVar()
+timer3 = tkinter.Label(top, textvariable=time3).grid(row=0, column=2)
+button3 = tkinter.Button().grid(row=1, column=2)
+
+time4 = tkinter.StringVar()
+timer4 = tkinter.Label(top, textvariable=time4).grid(row=0, column=3)
+button4 = tkinter.Button().grid(row=1, column=3)
 
 while True:
-    end_time = time.time()
-    time_lapsed = int(end_time - start_time)
-    print(time_calc(time_lapsed))
-    time.sleep(1)
+    top.update_idletasks()
+    top.update()
+    time1.set(time_calc(int(time.time() - start_time)))
+    time2.set(time_calc(int(time.time() - start_time - 1)))
+    time3.set(time_calc(int(time.time() - start_time - 2)))
+    time4.set(time_calc(int(time.time() - start_time - 3)))
